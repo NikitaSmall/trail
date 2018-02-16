d64f6e550cbb89bad3d3b316a34c324d81fa4694
NikitaSmall <nikitasosnov@yahoo.com>
2018-02-16 14:01:44 +0200

    rubocop update

--- lib/trail.rb
+++ lib/trail.rb
@@ -1,5 +1,6 @@
 require 'trail/version'
 
+# Trail is a gem that works with Testrail projects
 module Trail
   autoload :Config, 'trail/config'
   autoload :Client, 'trail/client'
--- lib/trail/config.rb
+++ lib/trail/config.rb
@@ -1,4 +1,6 @@
 module Trail
+  # Main configuration class,
+  # it holds base configuration required for Testrail integration
   class Config
     attr_accessor :base_url, :username, :password, :yaml_file
   end
